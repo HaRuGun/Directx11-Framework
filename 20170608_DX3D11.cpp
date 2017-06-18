93754c4f72b80d410ad1a83a43c6acb36f85f95c
HaRu <vharugun9093@gmail.com>
2017-06-18 18:16:33 +0900

    Allow Full-Screen Mode

--- 20170608_DX3D11.cpp
+++ 20170608_DX3D11.cpp
@@ -1,6 +1,15 @@
 #include "stdafx.h"
 
 
+IDXGISwapChain *swapChain;
+ID3D11Device *dev;
+ID3D11DeviceContext *devCon;
+ID3D11RenderTargetView *backBuffer;
+
+void InitD3D(HWND hWnd);
+void RenderFrame();
+void CleanD3D();
+
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 
@@ -16,18 +25,22 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	wc.lpfnWndProc = WindowProc;
 	wc.hInstance = hInstance;
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	// wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
 	wc.lpszClassName = "WindowClass";
 
 	RegisterClassEx(&wc);
 
-	RECT wr = { 0, 0, 1280, 720 };
+	RECT wr = { 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT };
 	AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);
 
-	hWnd = CreateWindowEx(NULL, "WindowClass", "Framework DX11", WS_OVERLAPPEDWINDOW,
-		100, 50, wr.right - wr.left, wr.bottom - wr.top, NULL, NULL, hInstance, NULL);
+	hWnd = CreateWindowEx(
+		NULL, "WindowClass", "Framework DX11", WS_OVERLAPPEDWINDOW,
+		100, 50, wr.right - wr.left, wr.bottom - wr.top,
+		NULL, NULL, hInstance, NULL);
 
 	ShowWindow(hWnd, nCmdShow);
+
+	InitD3D(hWnd);
 
 	MSG msg = { 0 };
 
@@ -38,13 +51,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 
-			if (msg.message == WM_QUIT) break;
+			if (msg.message == WM_QUIT)
+				break;
 		}
-		else
-		{
-			//RUN
-		}
+		
+		RenderFrame();
 	}
+
+	CleanD3D();
 
 	return msg.wParam;
 }
@@ -61,4 +75,71 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
 	}
 
 	return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+
+void InitD3D(HWND hWnd)
+{
+	// Direct3D Init
+	DXGI_SWAP_CHAIN_DESC scd;
+
+	ZeroMemory(&scd, sizeof(DXGI_SWAP_CHAIN_DESC));
+
+	scd.BufferCount = 1;
+	scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	scd.BufferDesc.Width = SCREEN_WIDTH;
+	scd.BufferDesc.Height = SCREEN_HEIGHT;
+	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	scd.OutputWindow = hWnd;
+	scd.SampleDesc.Count = 4;
+	scd.Windowed = TRUE;
+	scd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+
+	D3D11CreateDeviceAndSwapChain(
+		NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, NULL, 
+		NULL, NULL, D3D11_SDK_VERSION, &scd,
+		&swapChain, &dev, NULL, &devCon);
+
+	// Set Render Target
+	ID3D11Texture2D *pBackBuffer;
+	swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+
+	dev->CreateRenderTargetView(pBackBuffer, NULL, &backBuffer);
+	pBackBuffer->Release();
+
+	devCon->OMSetRenderTargets(1, &backBuffer, NULL);
+
+	// Set Viewport
+	D3D11_VIEWPORT viewPort;
+	ZeroMemory(&viewPort, sizeof(D3D11_VIEWPORT));
+
+	viewPort.TopLeftX = 0;
+	viewPort.TopLeftY = 0;
+	viewPort.Width = SCREEN_WIDTH;
+	viewPort.Height = SCREEN_HEIGHT;
+
+	devCon->RSSetViewports(1, &viewPort);
+}
+
+
+void RenderFrame()
+{
+	devCon->ClearRenderTargetView(backBuffer, D3DXCOLOR(0.4f, 0.6f, 0.8f, 1.0f));
+	// [START]
+
+
+
+	// [END]
+	swapChain->Present(0, 0);
+}
+
+
+void CleanD3D()
+{
+	swapChain->SetFullscreenState(FALSE, NULL);
+
+	swapChain->Release();
+	backBuffer->Release();
+	dev->Release();
+	devCon->Release();
 }
--- stdafx.h
+++ stdafx.h
@@ -20,3 +20,4 @@
 #pragma comment (lib, "d3dx11.lib")
 #pragma comment (lib, "d3dx10.lib")
 
+#include "20170608_DX3D11.h"
